d955d761f4a90bf6bf3f71ed933bcc93fbce9755
CJ Avilla <cjavilla@gmail.com>
2013-12-10 11:03:02 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aditup::Application.config.secret_token = '670bcf1c4034fa3932b869aadb523a4517af7dfe9a4092e72ae778ec37e2c84ada970e37ee6016fd9865b35dc103ea36ef656b4071b636d7cf9085972bbf33bb'
