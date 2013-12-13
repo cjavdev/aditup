98ac611cca7eef4080cf0c8598ce9a4bbd1e75e2
CJ Avilla <cjavilla@gmail.com>
2013-12-12 16:58:04 -0800

    formatting

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,5 @@ class Client < ActiveRecord::Base
   attr_accessible :account_manager_id, :name
 
   belongs_to :account_manager
+  has_many :spots
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,9 @@
 class Spot < ActiveRecord::Base
-  attr_accessible :buy_value, :client_id, :paid_views_left, :video_url, :view_value
+  attr_accessible :buy_value,
+                  :client_id,
+                  :paid_views_left,
+                  :video_url,
+                  :view_value
+
+  belongs_to :client
 end
