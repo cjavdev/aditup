d4615bec58fed76ac1c1e13a7ea0df6e3f1e9472
CJ Avilla <cjavilla@gmail.com>
2013-12-27 20:27:14 -0800

    adding mailing stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Aditup::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { :host => "aditup.herokuapp.com" }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
